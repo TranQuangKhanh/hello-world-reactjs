f81180bb69c08bdb85ed88f9c0114f1a85498635
TranQuangKhanh <quangkhanh1029@gmail.com>
2025-10-07 01:06:41 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello World 🌍</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 style={{ color: "#fff", fontSize: "40px" }}>Welcome to My Page</h1>
+        <p style={{ color: "#f0f0f0", fontSize: "18px", marginTop: "20px" }}>
+          This is my student id 23520724
+        </p>
+
+        <div style={{ marginTop: "50px" }}>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/00/Logo_UIT_updated.svg"
+            alt="React Logo"
+            width="500"
+            height="500git inirt"
+            className="App-logo"
+          />
+        </div>
+      </header>
     </div>
   );
 }
